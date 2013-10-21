1d99a8641da3bb79dd82991afb84e89c0ccded16
JarnoNijboer <git@nijboer.me>
2013-10-21 20:25:53 +0200

    Inital commit

---
+++ Skiwy.Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Skiwy.Data.dll")]
+[assembly: AssemblyDescription("Skiwy - IIS Express Manager")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jarno Nijboer")]
+[assembly: AssemblyProduct("Skiwy.Data.dll")]
+[assembly: AssemblyCopyright("Copyright © Jarno Nijboer 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6840a0b2-8773-4503-aa84-8363238a699d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
